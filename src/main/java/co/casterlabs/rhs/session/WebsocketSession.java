a5d55d226ded71b03a014401cc9a63f1145b51f4
Ender <33337309+e3ndr@users.noreply.github.com>
2023-11-19 21:49:50 -0600

    Fix some confusion with types.

--- src/main/java/co/casterlabs/rhs/session/WebsocketSession.java
+++ src/main/java/co/casterlabs/rhs/session/WebsocketSession.java
@@ -3,35 +3,16 @@ package co.casterlabs.rhs.session;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.jetbrains.annotations.Nullable;
-
 public abstract class WebsocketSession extends HttpSession {
 
-    /**
-     * @deprecated Websockets do not support this.
-     */
-    @Deprecated
     @Override
     public boolean hasBody() {
         return false;
     }
 
-    /**
-     * @deprecated Websockets do not support this.
-     */
-    @Deprecated
-    @Override
-    public @Nullable byte[] getRequestBodyBytes() throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * @deprecated Websockets do not support this.
-     */
-    @Deprecated
     @Override
     public InputStream getRequestBodyStream() throws IOException {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Websockets do not support request bodies.");
     }
 
 //    /**
@@ -42,14 +23,5 @@ public abstract class WebsocketSession extends HttpSession {
 //    public Either<MultipartForm, URLEncodedForm> parseFormBody() throws IOException {
 //        throw new UnsupportedOperationException();
 //    }
-
-    /**
-     * @deprecated Websockets do not support this.
-     */
-    @Deprecated
-    @Override
-    public String getRawMethod() {
-        return "GET";
-    }
 
 }
